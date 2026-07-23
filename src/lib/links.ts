27dfd84f9528dde0e3c3ab4fb09d3e34151b5382
rinaxosman <94803092+rinaxosman@users.noreply.github.com>
2026-07-22 20:33:32 -0400

    add resume

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -3,7 +3,7 @@ export const LINKS = {
   email: "rinaxosman@gmail.com", // change to your email
   linkedin: "https://www.linkedin.com/in/rina-osman/", // change to your linkedin profile!
   github: "https://github.com/rinaxosman", // change to your github profile
-  resume: "/Rina-Osman-Resume.pdf", // PDF in /public
+  resume: "/rina-26-un.pdf", // PDF in /public
   substack: "https://rinaosman.substack.com/feed", // modify or remove
   repo: "https://github.com/rinaxosman/nextjs-portfolio",
   honoursReport: "/CSI4900-VulRepair.pdf"
